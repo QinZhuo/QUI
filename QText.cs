8dd4966440e3440a5fc8f6e67bb8af1dacf55d72
琴卓 <1043665802@qq.com>
2020-10-07 21:00:00 +0800

    初始化

---
+++ QText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+namespace QTool.UI
+{
+    public class QText : MonoBehaviour
+    {
+        private static readonly Regex _regex = new Regex(@"{(.+?)=(.+?)}", RegexOptions.Singleline);
+        [ContextMenu("Test")]
+        public void Test()
+        {
+
+        }
+    }
+}

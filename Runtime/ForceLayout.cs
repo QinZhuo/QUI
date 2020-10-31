0124fb53899404c96b00dab4400ce42f8fec5173
琴卓 <1043665802@qq.com>
2020-10-31 16:00:22 +0800

    0.0.6 新增对象池布局相关逻辑

---
+++ Runtime/ForceLayout.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+namespace QTool.UI
+{
+    public class ForceLayout : MonoBehaviour
+    {
+        [System.Serializable]
+        public class ActionEvent : UnityEvent
+        {
+        }
+        public ActionEvent OnLayout;
+        [ContextMenu("布局")]
+        public void Layout()
+        {
+            StartCoroutine(WaitLayout());
+        }
+        public int DelayLayout = 1;
+        IEnumerator WaitLayout()
+        {
+            for (int i = 0; i < DelayLayout; i++)
+            {
+                yield return null;
+                LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
+                OnLayout?.Invoke();
+
+            }
+
+        }
+
+    }
+}
+
---
+++ Runtime/LayoutViewContent.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace QTool.UI
+{
+    public class LayoutViewContent : ForceLayout
+    {
+
+        public QTool.ObjectPool<GameObject> GetPool(int index)
+        {
+            return PoolManager.GetPool(viewPrefab[index]);
+        }
+        private void Awake()
+        {
+            objList = new List<GameObject>[viewPrefab.Length];
+            for (int i = 0; i < objList.Length; i++)
+            {
+                objList[i] = new List<GameObject>();
+            }
+            
+           
+        }
+        public GameObject[] viewPrefab;
+        public virtual GameObject this[string name, int index = 0]
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    Debug.LogError(this + "索引为空[" + name + "]");
+                    return null;
+                }
+                var trans = transform.Find(name);
+                var view = (trans != null && trans.gameObject.activeSelf) ? trans.gameObject : null;
+                if (view == null)
+                {
+                    view = GetPool(index).Get();
+                    view.transform.SetParent(transform);
+                    view.transform.SetAsLastSibling();
+                    objList[index].Add(view);
+                    view.name = name;
+                    OnCreate?.Invoke(view);
+                    Layout();
+                }
+                return view;
+            }
+        }
+        List<GameObject>[] objList;
+        public virtual void Clear()
+        {
+            for (int i = 0; i < objList.Length; i++)
+            {
+                foreach (var view in objList[i])
+                {
+                    GetPool(i).Push(view);
+                }
+                objList[i].Clear();
+
+            }
+            OnClear?.Invoke();
+            Layout();
+        }
+        public event System.Action<GameObject> OnCreate;
+        public event System.Action OnClear;
+    }
+
+}

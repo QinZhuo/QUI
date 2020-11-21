5bb7d744b2be25eeffecaf102c55837dcf4228a2
琴卓 <1043665802@qq.com>
2020-11-21 15:23:31 +0800

    更新范围判断

--- Runtime/ForceLayout.cs
+++ Runtime/ForceLayout.cs
@@ -9,34 +9,34 @@ namespace QTool.UI
     {
         public static Vector2 UpRightRectOffset(this RectTransform rectTransform)
         {
-            return new Vector2(rectTransform.sizeDelta.x * (1 - rectTransform.pivot.x), rectTransform.sizeDelta.y * (1 - rectTransform.pivot.y));
+            return new Vector2(rectTransform.Width() * (1 - rectTransform.pivot.x), rectTransform.Height() * (1 - rectTransform.pivot.y));
         }
         public static Vector2 DownLeftRectOffset(this RectTransform rectTransform)
         {
-            return new Vector2(rectTransform.sizeDelta.x * (rectTransform.pivot.x), rectTransform.sizeDelta.y * (rectTransform.pivot.y));
+            return new Vector2(rectTransform.Width() * (rectTransform.pivot.x), rectTransform.Height() * (rectTransform.pivot.y));
         }
 
         public static float Height(this RectTransform rectTransform)
         {
-            return rectTransform.sizeDelta.y;
+            return rectTransform.rect.size.y;
         }
         public static float Width(this RectTransform rectTransform)
         {
-            return rectTransform.sizeDelta.x;
+            return rectTransform.rect.size.x;
         }
-        public static bool IsOutRange(this RectTransform transform,RectTransform mask)
+        public static bool IsOutRange(this RectTransform transform, RectTransform mask)
         {
-            if (transform.Left() > mask.Right()||transform.Right()<transform.Left())
+            if (transform.Left() > mask.Right() || transform.Right() < transform.Left())
             {
                 return true;
             }
-            else if(transform.Up()<mask.Down()||transform.Down()>mask.Up())
+            else if (transform.Up() < mask.Down() || transform.Down() > mask.Up())
             {
                 return true;
             }
             return false;
         }
-   
+
         public static RectTransform RectTransform(this Transform transform)
         {
             return transform as RectTransform;

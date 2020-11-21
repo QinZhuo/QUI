1bbfdeebe675826c6ceb119f9eda37692c9e36d0
琴卓 <1043665802@qq.com>
2020-11-21 14:30:42 +0800

    更新

--- Runtime/ForceLayout.cs
+++ Runtime/ForceLayout.cs
@@ -5,6 +5,46 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 namespace QTool.UI
 {
+    public static class RectTransformExtend
+    {
+        public static Vector2 UpRightRectOffset(this RectTransform rectTransform)
+        {
+            return new Vector2(rectTransform.sizeDelta.x * (1 - rectTransform.pivot.x), rectTransform.sizeDelta.y * (1 - rectTransform.pivot.y));
+        }
+        public static Vector2 DownLeftRectOffset(this RectTransform rectTransform)
+        {
+            return new Vector2(rectTransform.sizeDelta.x * (rectTransform.pivot.x), rectTransform.sizeDelta.y * (rectTransform.pivot.y));
+        }
+
+        public static float Height(this RectTransform rectTransform)
+        {
+            return rectTransform.sizeDelta.y;
+        }
+        public static float Width(this RectTransform rectTransform)
+        {
+            return rectTransform.sizeDelta.x;
+        }
+        public static RectTransform RectTransform(this Transform transform)
+        {
+            return transform as RectTransform;
+        }
+        public static float Up(this RectTransform rectTransform)
+        {
+            return rectTransform.transform.position.y + rectTransform.UpRightRectOffset().y;
+        }
+        public static float Down(this RectTransform rectTransform)
+        {
+            return rectTransform.transform.position.y - rectTransform.DownLeftRectOffset().y;
+        }
+        public static float Left(this RectTransform rectTransform)
+        {
+            return rectTransform.transform.position.x - rectTransform.DownLeftRectOffset().x;
+        }
+        public static float Right(this RectTransform rectTransform)
+        {
+            return rectTransform.transform.position.x + rectTransform.UpRightRectOffset().x;
+        }
+    }
     public class ForceLayout : MonoBehaviour
     {
         [System.Serializable]

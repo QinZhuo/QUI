ee0645bee248abd2ae93ce4ef3e868bb899740b4
琴卓 <1043665802@qq.com>
2021-02-25 18:15:26 +0800

    增加Qrectangle相关逻辑

---
+++ Runtime/QRectangle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class QRectangle :MonoBehaviour
+{
+    public Image image;
+    public float circle=10;
+    protected void Reset()
+    {
+        image = GetComponent<Image>();
+        image.sprite = Resources.Load<Sprite>("QCircle512");
+        image.type = Image.Type.Sliced;
+        FreshCircle();
+    }
+    public void FreshCircle()
+    {
+        image.pixelsPerUnitMultiplier = circle == 0 ? 0.0001f : 256 / circle;
+        image.SetVerticesDirty();
+    }
+    protected void OnValidate()
+    {
+        FreshCircle();
+    }
+}
+   
+  

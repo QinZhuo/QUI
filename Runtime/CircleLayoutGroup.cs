6fd6a49828562db4765e44f5559a8613a9f5619c
琴卓 <1043665802@qq.com>
2021-04-02 18:24:52 +0800

    增加旋转逻辑

--- Runtime/CircleLayoutGroup.cs
+++ Runtime/CircleLayoutGroup.cs
@@ -22,8 +22,10 @@ namespace QTool.UI
         }
         public float startAngle = 0;
         public bool reverse = false;
+        public bool rotate = false;
         public float distance = 20;
         private Vector2[] childPositions = new Vector2[0];
+        private Quaternion[] childRotations = new Quaternion[0];
         public override void CalculateLayoutInputVertical()
         {
             FreshCirclePos();
@@ -42,17 +44,27 @@ namespace QTool.UI
                 RectTransform child = rectChildren[i];
                 SetChildAlongAxis(child, 0, childPositions[i].x);
                 SetChildAlongAxis(child, 1, childPositions[i].y);
+                if (rotate)
+                {
+                    rectChildren[i].rotation = childRotations[i];
+                }
             }
         }
         private void FreshCirclePos()
         {
             childPositions = new Vector2[rectChildren.Count];
-            var start = (-90 + startAngle) / 360 * Mathf.PI * 2;
+            childRotations = new Quaternion[rectChildren.Count];
+            var start = (-90+startAngle) / 360 * Mathf.PI * 2;
             var dis = distance == 0 ? 0 : distance / 360 * Mathf.PI * 2;
             for (int i = 0; i < rectChildren.Count; i++)
             {
                 var angle = start + (reverse ? 1 : -1) * i * dis;
-                childPositions[i] = Center + new Vector2(Mathf.Cos(angle) * Size.x, Mathf.Sin(angle) * Size.y) / 2 - rectChildren[i].sizeDelta / 2;
+                var pos= new Vector2(Mathf.Cos(angle) * Size.x, Mathf.Sin(angle) * Size.y) / 2 - rectChildren[i].sizeDelta / 2;
+                childPositions[i] = Center + pos;
+                if (rotate)
+                {
+                    childRotations[i] = Quaternion.Euler(0, 0, -angle / Mathf.PI * 180 - 90);
+                }
             }
         }
     }

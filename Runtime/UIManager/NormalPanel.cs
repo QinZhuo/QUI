578176e4a424c79bc54ff83735ddc822b8ec925e
琴卓 <1043665802@qq.com>
2021-09-26 15:09:46 +0800

    增加debug信息

--- Runtime/UIManager/NormalPanel.cs
+++ Runtime/UIManager/NormalPanel.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace QTool.UI
 {
+    public class UIDebug:DebugBase<UIDebug>
+    {
+
+    }
     public class NormalPanel : UIPanel<NormalPanel>
     {
+        protected override void OnShow()
+        {
+            base.OnShow();
+            UIDebug.Log("ÏÔÊ¾Panel:¡¾" + this + "¡¿");
+        }
+        protected override void OnHide()
+        {
+            base.OnHide();
+            UIDebug.Log("Òþ²ØPanel:¡¾" + this + "¡¿");
+        }
     }
 }
 

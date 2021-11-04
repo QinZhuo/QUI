7c3052d907ef3641aebcbfecf8e7040255ffb192
琴卓 <1043665802@qq.com>
2021-11-05 00:01:16 +0800

    删除debug

--- Runtime/UIManager/NormalPanel.cs
+++ Runtime/UIManager/NormalPanel.cs
@@ -5,16 +5,16 @@ namespace QTool.UI
 {
     public class NormalPanel : UIPanel<NormalPanel>
     {
-        protected override void OnShow()
-        {
-            base.OnShow();
-            Debug.Log("ÏÔÊ¾Panel:¡¾" + this + "¡¿");
-        }
-        protected override void OnHide()
-        {
-            base.OnHide();
-            Debug.Log("Òþ²ØPanel:¡¾" + this + "¡¿");
-        }
+        //protected override void OnShow()
+        //{
+        //    base.OnShow();
+        //    //Debug.Log("ÏÔÊ¾Panel:¡¾" + this + "¡¿");
+        //}
+        //protected override void OnHide()
+        //{
+        //    base.OnHide();
+        //    //Debug.Log("Òþ²ØPanel:¡¾" + this + "¡¿");
+        //}
     }
 }
 

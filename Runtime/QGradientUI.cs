dd8a4b515ad1b93a1dec13b66e57772e370de38c
QinZhuo <1043665802@qq.com>
2021-11-18 18:42:35 +0800

    更新

--- Runtime/QGradientUI.cs
+++ Runtime/QGradientUI.cs
@@ -221,14 +221,15 @@ namespace QTool.UI
         public void LerpColor(VertexHelper vh)
         {
             var length = maxValue - minValue;
-            if (length == float.NaN)
-            {
-                return;
-            }
+         
             for (int i = 0; i < vh.currentVertCount; i++)
             {
                 vh.PopulateUIVertex(ref v, i);
                 var t = (GetValue(v, i) - minValue) / length;
+                if (t==float.NaN)
+                {
+                    return;
+                }
                 var newColor = v.color* GradientColor.Evaluate(t);
                 if (v.color != newColor)
                 {

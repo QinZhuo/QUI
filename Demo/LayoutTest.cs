e09069396535f0c4a68e3539eb03f1c8dadd0c1b
琴卓 <1043665802@qq.com>
2022-03-19 00:44:16 +0800

    命名空间bug

--- Demo/LayoutTest.cs
+++ Demo/LayoutTest.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using QTool.UI;
-public class LayoutTest : UIPanel<LayoutTest>
+using QTool;
+namespace QTool.UI
 {
-    public QObjectList testList;
-    // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            var view= testList[i.ToString()];
-            view.transform.localScale = Vector3.zero;
-        }
-       
-    }
 
-   
+    public class LayoutTest : UIPanel<LayoutTest>
+    {
+        public QObjectList testList;
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var view = testList[i.ToString()];
+                view.transform.localScale = Vector3.zero;
+            }
+
+        }
+    }
 }

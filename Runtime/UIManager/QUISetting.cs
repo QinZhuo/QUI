c50a0a6cc66d76a8f7b8b765884bd14acd17d415
琴卓 <1043665802@qq.com>
2022-06-04 19:10:06 +0800

    WaitInitOver

--- Runtime/UIManager/QUISetting.cs
+++ Runtime/UIManager/QUISetting.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using QTool.Inspector;
+using System.Threading.Tasks;
+
 namespace QTool.UI
 {
-    public class QUISetting:InstanceBehaviour<QUISetting>
-    {
-        public static List<string> curList = new List<string>();
-        [ViewName("UI配置")]
-        public List<string> PanelList;
-        public bool InitOver { private set; get; } = false;
+	public class QUISetting : InstanceBehaviour<QUISetting>
+	{
+		public static List<string> curList = new List<string>();
+		[ViewName("UI配置")]
+		public List<string> PanelList;
+		public bool InitOver { private set; get; } = false;
+		public static async Task<bool> WaitInitOver()
+		{
+			if (Instance == null)
+			{
+				return true;
+			}
+			else
+			{
+				return await Tool.Wait(() => Instance.InitOver);
+			}
+		}
         protected async  void Start()
         {
             InitOver = false;

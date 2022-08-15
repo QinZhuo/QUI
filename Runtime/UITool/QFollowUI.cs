5c10df83fa1a987dc2da1d01c1e52293f30ef0e6
琴卓 <1043665802@qq.com>
2022-08-15 17:35:59 +0800

    完善follow逻辑

--- Runtime/UITool/QFollowUI.cs
+++ Runtime/UITool/QFollowUI.cs
@@ -6,11 +6,14 @@ namespace QTool.UI
     public class QFollowUI : MonoBehaviour
     {
         public Transform target;
+		[ViewName("使用包围盒高度")]
+		public bool useBoundsHeight=false;
+		public Vector3 offset=Vector3.zero;
         private void LateUpdate()
         {
             if (target != null)
             {
-                transform.position = Camera.main.WorldToScreenPoint(target.position);
+                transform.position = Camera.main.WorldToScreenPoint(target.position+ offset + Vector3.up* transform.GetBounds().max.y*(useBoundsHeight ? 1:0));
             }
         }
 		public void Recover()

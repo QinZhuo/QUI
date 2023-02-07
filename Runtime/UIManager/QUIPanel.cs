f95273e0eabe70ec4a5193d786ed6c577144ccbd
琴卓 <1043665802@qq.com>
2023-02-07 17:55:56 +0800

    IsPlaying

--- Runtime/UIManager/QUIPanel.cs
+++ Runtime/UIManager/QUIPanel.cs
@@ -298,7 +298,7 @@ namespace QTool.UI
 				if (IsShow != base.IsShow) {
 					return;
 				}
-				if (Application.isPlaying)
+				if (Application.IsPlaying(this))
 				{
 					gameObject.SetActive(IsShow);
 				}
@@ -307,7 +307,7 @@ namespace QTool.UI
 #endif
 			{
 				if (this == null) return;
-				if (Application.isPlaying)
+				if (Application.IsPlaying(this))
 				{
 					gameObject.SetActive(IsShow);
 				}

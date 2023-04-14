ad9af0dbcc410560675c503b397ebe24b9dd96a7
琴卓 <1043665802@qq.com>
2023-04-14 18:33:30 +0800

    预加载UI日志

--- Runtime/UIManager/QSceneUISetting.cs
+++ Runtime/UIManager/QSceneUISetting.cs
@@ -8,16 +8,18 @@ namespace QTool.UI
 {
 	public class QSceneUISetting : InstanceBehaviour<QSceneUISetting>
 	{
-		[QName("初始化UI")]
+		[QName("预加载UI")]
 		[QEnum(nameof(QUIPanelPrefab) +"."+nameof(QUIPanelPrefab.LoadAll))]
 		public List<string> PanelList = new List<string>();
 		protected override void Awake()
 		{
 			base.Awake();
+			var startTime = QDebug.Timestamp;
 			foreach (var uiKey in PanelList)
 			{
 				QUIManager.GetUI(uiKey)?.ResetUI();
 			}
+			QDebug.Log("预加载场景UI完成", startTime);
 		}
 	}
 }

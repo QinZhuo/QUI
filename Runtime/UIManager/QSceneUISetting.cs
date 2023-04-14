e782c0d455e6733718f030080faa054982bc40bb
琴卓 <1043665802@qq.com>
2023-04-14 21:22:41 +0800

    完善预加载相关逻辑

--- Runtime/UIManager/QSceneUISetting.cs
+++ Runtime/UIManager/QSceneUISetting.cs
@@ -11,10 +11,19 @@ namespace QTool.UI
 		[QName("预加载UI")]
 		[QEnum(nameof(QUIPanelPrefab) +"."+nameof(QUIPanelPrefab.LoadAll))]
 		public List<string> PanelList = new List<string>();
-		protected override void Awake()
+		private void Start()
 		{
-			base.Awake();
+			QTool.PreLoadTaskList.Add(PreLoad());
+		}
+		private async Task PreLoad()
+		{
 			var startTime = QDebug.Timestamp;
+			var taskList = new List<Task>();
+			foreach (var uiKey in PanelList)
+			{
+				taskList.Add(QUIPanelPrefab.LoadAsync(uiKey));
+			}
+			await taskList.WaitAllOver();
 			foreach (var uiKey in PanelList)
 			{
 				QUIManager.GetUI(uiKey)?.ResetUI();

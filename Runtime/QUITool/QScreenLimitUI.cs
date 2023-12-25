86c963b8addb02af6dd90892dde46d46294c99fe
琴卓 <1043665802@qq.com>
2023-12-26 03:30:40 +0800

    删除 Runtime/QUITool/QRangeLimitUI.cs 删除 Runtime/QUITool/QRangeLimitUI.cs.meta 修改 package.json 新增 Runtime/QUITool/QScreenLimitUI.cs 新增 Runtime/QUITool/QScreenLimitUI.cs.meta

--- Runtime/QUITool/QScreenLimitUI.cs
+++ Runtime/QUITool/QScreenLimitUI.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 namespace QTool.UI
 {
 	[ExecuteInEditMode]
-	public class QRangeLimitUI : MonoBehaviour
+	public class QScreenLimitUI : MonoBehaviour
 	{
 		private void Update()
 		{
 			var parentRect = transform.parent as RectTransform;
 			var rect = transform as RectTransform;
 			var offset = rect.UpRight() - parentRect.UpRight();
-			rect.anchoredPosition -= new Vector2(offset.x > 0 ? offset.x : 0, offset.y > 0 ? offset.y : 0);
+			rect.position -= new Vector3(offset.x > 0 ? offset.x : 0, offset.y > 0 ? offset.y : 0);
 			offset = rect.DownLeft() - parentRect.DownLeft();
-			rect.anchoredPosition -= new Vector2(offset.x < 0 ? offset.x : 0, offset.y < 0 ? offset.y : 0);
+			rect.position -= new Vector3(offset.x < 0 ? offset.x : 0, offset.y < 0 ? offset.y : 0);
 		}
 	}
 }

62e5214bb65951326b74be3aebaae3345ec4f730
琴卓 <1043665802@qq.com>
2024-01-07 13:35:02 +0800

    修改 Demo/Scenes/TestScene.unity 修改 Runtime/QUITool/QCircleLayoutGroup.cs

--- Runtime/QUITool/QCircleLayoutGroup.cs
+++ Runtime/QUITool/QCircleLayoutGroup.cs
@@ -55,43 +55,38 @@ namespace QTool.UI
 		}
 		private void FreshCirclePos()
 		{
+			var Spacing = this.Spacing;
+			if (Spacing <= 0)
+			{
+				Spacing = 360f / rectChildren.Count;
+			}
 			var startAngle = 0f;
 			var reverse = false;
-			var center = false;
 			switch (childAlignment)
 			{
-				case TextAnchor.UpperLeft:
-					startAngle = -90 + padding.left;
-					break;
 				case TextAnchor.MiddleLeft:
-					startAngle = -180 + padding.left;
+				case TextAnchor.UpperLeft:
+					reverse = true;
+					startAngle = -90 + padding.left;
 					break;
 				case TextAnchor.LowerLeft:
 					startAngle = 90 + padding.left;
 					break;
-				case TextAnchor.UpperCenter:
-					startAngle = -90 + padding.left;
-					center = true;
-					break;
-				case TextAnchor.MiddleCenter:
-					startAngle = padding.left;
-					center = true;
-					break;
-				case TextAnchor.LowerCenter:
-					startAngle = 90 + padding.left;
-					center = true;
-					break;
 				case TextAnchor.UpperRight:
-					reverse = true;
-					startAngle = -90 + padding.right;
-					break;
 				case TextAnchor.MiddleRight:
-					reverse = true;
-					startAngle = padding.right;
+					startAngle = -90 + padding.right;
 					break;
 				case TextAnchor.LowerRight:
 					reverse = true;
 					startAngle = 90 + padding.right;
+					break;
+
+				case TextAnchor.MiddleCenter:
+				case TextAnchor.UpperCenter:
+					startAngle = -(90 + (rectChildren.Count - 1) * Spacing / 2) + padding.left;
+					break;
+				case TextAnchor.LowerCenter:
+					startAngle = 90 - (rectChildren.Count - 1) * Spacing / 2 + padding.left;
 					break;
 				default:
 					break;
@@ -99,19 +94,12 @@ namespace QTool.UI
 			childPositions = new Vector2[rectChildren.Count];
 			childRotations = new Quaternion[rectChildren.Count];
 			var start = startAngle / 360 * Mathf.PI * 2;
-			var offset = Spacing == 0 ? 0 : Spacing / 360 * Mathf.PI * 2;
+			
+			var offset = Spacing / 360 * Mathf.PI * 2;
+			
 			for (int i = 0; i < rectChildren.Count; i++)
 			{
-				if (center)
-				{
-					var pos = rectChildren.Count - 1 - i;
-					SetAngle(i, start + (pos % 2 == 0 ? -1 * pos : 1 * (pos + 1)) * offset / 2);
-				}
-				else
-				{
-					SetAngle(i, start + (reverse ? -1 : 1) * i * offset);
-				}
-
+				SetAngle(i, start + (reverse ? -1 : 1) * i * offset);
 			}
 		}
 		private void SetAngle(int i, float angle)

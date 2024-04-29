d0752ec3d67167d6280601ff07c4f68c14b46577
琴卓 <1043665802@qq.com>
2024-04-30 01:53:13 +0800

    修改 Runtime/Core/QUIManager.cs

--- Runtime/Core/QUIManager.cs
+++ Runtime/Core/QUIManager.cs
@@ -15,92 +15,26 @@ namespace QTool.UI
 	/// </summary>
 	public static class QUIManager
 	{
-		private static QDictionary<string, QUI> PanelList = new QDictionary<string, QUI>();
+		#region 管理
+		private static QDictionary<string, QUI> UIList = new QDictionary<string, QUI>();
+		public static event System.Action<QUI> OnCurrentWindowChange;
 		/// <summary>
 		/// 注册UI到管理器
 		/// </summary>
 		internal static void Add(QUI panel)
 		{
-			PanelList[panel.QName()] = panel;
+			UIList[panel.QName()] = panel;
 		}
 		internal static void Remove(QUI panel)
 		{
 			var key = panel.name;
-			if (!key.IsNull() && PanelList.ContainsKey(key))
+			if (!key.IsNull() && UIList.ContainsKey(key))
 			{
-				var ui = PanelList[key];
+				var ui = UIList[key];
 				if (ui == panel)
 				{
-					PanelList.RemoveKey(key);
+					UIList.RemoveKey(key);
 				}
-			}
-		}
-		/// <summary>
-		/// UI是否正在显示
-		/// </summary>
-		/// <param name="key">UI关键名</param>
-		/// <returns></returns>
-		public static bool IsShow(this System.Enum enumKey)
-		{
-			var key = enumKey?.ToString();
-			if (PanelList.ContainsKey(key)) return PanelList[key.ToString()].GetComponent<QUI>().IsShow;
-			return false;
-		}
-
-		public static async Task Switch(this System.Enum enumKey, bool show)
-		{
-			if (show == IsShow(enumKey))
-			{
-				return;
-			}
-			if (show)
-			{
-				await Load(enumKey.ToString()).ShowAsync();
-			}
-			else
-			{
-				await Load(enumKey.ToString()).HideAsync();
-			}
-		}
-		public static void Show(this System.Enum key)
-		{
-			if (IsShow(key)) return;
-			Load(key.ToString()).Show();
-		}
-		public static void Hide(this System.Enum key)
-		{
-			if (!IsShow(key)) return;
-			Load(key.ToString()).Hide();
-		}
-		internal static async Task<QUI> LoadAsync(string key)
-		{
-			await UI_Prefab.LoadAsync(key);
-			return Load(key);
-		}
-		internal static QUI Load(string key)
-		{
-			if (!QTool.IsPlaying) return null;
-			if (PanelList[key] == null)
-			{
-				QDebug.Begin("动态创建" + nameof(QUI) + "<" + key + ">");
-				var prefab = UI_Prefab.Load(key);
-				if (prefab == null)
-				{
-					QDebug.LogError("不存在UI预制体 [" + key + "]");
-					return null;
-				}
-				var panel = Object.Instantiate(prefab).GetComponent<QUI>(true);
-				panel.name = key;
-				QDebug.End("动态创建" + nameof(QUI) + "<" + key + ">");
-			}
-			return PanelList[key];
-		}
-		
-		public static int Count
-		{
-			get
-			{
-				return ModalWindowStack.Count;
 			}
 		}
 		public static List<QUI> ModalWindowStack { get; private set; } = new List<QUI>();
@@ -117,7 +51,6 @@ namespace QTool.UI
 			ModalWindowStack.Push(window);
 			OnCurrentWindowChange?.Invoke(ModalWindowStack.StackPeek());
 		}
-		public static event System.Action<QUI> OnCurrentWindowChange;
 
 		internal static void ModalWindowRemove(QUI window)
 		{
@@ -125,6 +58,63 @@ namespace QTool.UI
 			ModalWindowStack.Remove(window);
 			OnCurrentWindowChange?.Invoke(ModalWindowStack.StackPeek());
 		}
+		internal static QUI Load(string key)
+		{
+			if (!QTool.IsPlaying) return null;
+			if (UIList[key] == null)
+			{
+				QDebug.Begin("动态创建" + nameof(QUI) + "<" + key + ">");
+				var prefab = UI_Prefab.Load(key);
+				if (prefab == null)
+				{
+					QDebug.LogError("不存在UI预制体 [" + key + "]");
+					return null;
+				}
+				var panel = Object.Instantiate(prefab).GetComponent<QUI>(true);
+				panel.name = key;
+				QDebug.End("动态创建" + nameof(QUI) + "<" + key + ">");
+			}
+			return UIList[key];
+		}
+		internal static async Task<QUI> LoadAsync(string key)
+		{
+			await UI_Prefab.LoadAsync(key);
+			return Load(key);
+		}
+		#endregion
+		#region 枚举接口
+		public static void Show(this System.Enum key)
+		{
+			if (IsShow(key)) return;
+			Load(key.ToString()).Show();
+		}
+		public static void Hide(this System.Enum key)
+		{
+			if (!IsShow(key)) return;
+			Load(key.ToString()).Hide();
+		}
+		public static bool IsShow(this System.Enum enumKey)
+		{
+			var key = enumKey?.ToString();
+			if (UIList.ContainsKey(key)) return UIList[key.ToString()].GetComponent<QUI>().IsShow;
+			return false;
+		}
+		public static async Task Switch(this System.Enum enumKey, bool show)
+		{
+			if (show == IsShow(enumKey))
+			{
+				return;
+			}
+			if (show)
+			{
+				await Load(enumKey.ToString()).ShowAsync();
+			}
+			else
+			{
+				await Load(enumKey.ToString()).HideAsync();
+			}
+		}
+		#endregion
 	}
 
 

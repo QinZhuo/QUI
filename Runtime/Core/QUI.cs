df25a649cd8b79c8e109a22b920e8e48e85bc213
琴卓 <1043665802@qq.com>
2024-06-06 02:50:03 +0800

    修改 Runtime/Core/QUI.cs 修改 Runtime/Core/QUIManager.cs 修改 package.json

--- Runtime/Core/QUI.cs
+++ Runtime/Core/QUI.cs
@@ -8,70 +8,52 @@ using QTool.Reflection;
 using UnityEngine.Profiling.Memory.Experimental;
 using UnityEditor;
 using UnityEngine.UI;
-
-
-
-
-
 #if QTween
 using QTool.Tween;
 #endif
-namespace QTool.UI
-{
+namespace QTool.UI {
 	/// <summary>
 	/// 基础UI 自动注册事件
 	/// </summary>
-	public class QUI : MonoBehaviour
-	{
-		public virtual Task ShowAsync()
-		{
+	public class QUI : MonoBehaviour {
+		public virtual Task ShowAsync() {
 			IsShow = true;
 			gameObject.SetActive(true);
 			return Task.CompletedTask;
 		}
 
-		public virtual Task HideAsync()
-		{
+		public virtual Task HideAsync() {
 			IsShow = false;
 			gameObject.SetActive(false);
 			return Task.CompletedTask;
 		}
 #if UNITY_EDITOR
-		private void OnValidate()
-		{
+		private void OnValidate() {
 			gameObject.AutoAddPersistentListener(this);
 		}
 #endif
-		protected virtual void Awake()
-		{
+		protected virtual void Awake() {
 			QUIManager.Add(this);
 		}
 		[QName("隐藏")]
-		public void Hide()
-		{
+		public void Hide() {
 			_ = HideAsync();
 		}
 		[QName("显示")]
-		public void Show()
-		{
+		public void Show() {
 			_ = ShowAsync();
 		}
-		public void Switch(bool show)
-		{
-			if (show)
-			{
+		public void Switch(bool show) {
+			if (show) {
 				Show();
 			}
-			else
-			{
+			else {
 				Hide();
 			}
 		}
 		public bool IsShow { internal set; get; }
-		public RectTransform RectTransform
-		{
-			get
-			{
+		public RectTransform RectTransform {
+			get {
 				return transform as RectTransform;
 			}
 		}
@@ -80,21 +62,19 @@ namespace QTool.UI
 		const string UICodeStart = "//UICodeStart\n";
 		const string UICodeEnd = "//UICodeEnd\n";
 		[QName("UI字段生成", nameof(CanGenerate))]
-		private void GenerateUICode()
-		{
+		private void GenerateUICode() {
 			var target = this;
-			if (!CanGenerate) return;
+			if (!CanGenerate)
+				return;
 			var script = UnityEditor.MonoScript.FromMonoBehaviour(this);
 			var path = UnityEditor.AssetDatabase.GetAssetPath(script);
 			var className = script.GetClass().Name;
 			var data = QFileTool.Load(path);
 			var oldUICode = "";
-			if (data.Contains(UICodeStart) && data.Contains(UICodeEnd))
-			{
+			if (data.Contains(UICodeStart) && data.Contains(UICodeEnd)) {
 				oldUICode = data.GetBlockValue(UICodeStart, UICodeEnd);
 			}
-			else
-			{
+			else {
 				var classIndex = data.IndexOf("class " + className);
 				var index = data.IndexOf('{', classIndex);
 				var startIndex = data.Substring(0, index).LastIndexOf('\n') + 1;
@@ -103,17 +83,14 @@ namespace QTool.UI
 				oldUICode = data.GetBlockValue(UICodeStart, UICodeEnd);
 			}
 			var start = oldUICode.Substring(oldUICode.LastIndexOf('\n'));
-			var newUICode = QTool.BuildString(writer =>
-			{
+			var newUICode = QTool.BuildString(writer => {
 				var texts = gameObject.GetComponentsInChildren<Text>();
-				foreach (var item in texts)
-				{
+				foreach (var item in texts) {
 					writer.Write(string.Format("{0}public Text {1}=null;\n", start, item.name.Replace(" ", "_")));
 				}
 			});
 			newUICode += start;
-			if (oldUICode != newUICode)
-			{
+			if (oldUICode != newUICode) {
 				data = data.Replace(UICodeStart + oldUICode + UICodeEnd, UICodeStart + newUICode + UICodeEnd);
 				Debug.LogError(data);
 			}
@@ -124,53 +101,41 @@ namespace QTool.UI
 	}
 
 	[RequireComponent(typeof(CanvasGroup))]
-	public abstract class QUI<T> : QUI where T : QUI<T>
-	{
+	public abstract class QUI<T> : QUI where T : QUI<T> {
 		#region 静态逻辑
 		private static T _instance;
-		public static T Instance
-		{
-			get
-			{
-				if (_instance != null || !Application.isPlaying) return _instance;
+		public static T Instance {
+			get {
+				if (_instance != null || !Application.isPlaying)
+					return _instance;
 				_instance = QUIManager.Load(typeof(T).Name) as T;
 				return _instance;
 			}
 		}
-		public static bool PanelIsShow
-		{
-			get
-			{
-				if (_instance == null)
-				{
+		public static bool PanelIsShow {
+			get {
+				if (_instance == null) {
 					return false;
 				}
-				else
-				{
+				else {
 					return Instance.IsShow;
 				}
 			}
 		}
-		public static async Task LoadAsync()
-		{
+		public static async Task LoadAsync() {
 			await QUIManager.LoadAsync(typeof(T).Name);
 		}
-		public static void ShowPanel()
-		{
+		public static void ShowPanel() {
 			_ = ShowPanelAsync();
 		}
-		public static void HidePanel()
-		{
+		public static void HidePanel() {
 			_ = HidePanelAsync();
 		}
-		public static async Task ShowPanelAsync()
-		{
+		public static async Task ShowPanelAsync() {
 			await Instance?.ShowAsync();
 		}
-		public static async Task HidePanelAsync()
-		{
-			if (PanelIsShow)
-			{
+		public static async Task HidePanelAsync() {
+			if (PanelIsShow) {
 				await Instance?.HideAsync();
 			}
 		}
@@ -191,136 +156,105 @@ namespace QTool.UI
 		#region 基本生命周期
 
 		private int initIndex = -1;
-		protected override void Awake()
-		{
-			if (this is T panel)
-			{
+		protected override void Awake() {
+			if (this is T panel) {
 				_instance = this as T;
 			}
-			else
-			{
+			else {
 				Debug.LogError("页面类型不匹配：" + _instance + ":" + typeof(T));
 			}
 			IsShow = Group.alpha >= 0.9f;
 			QUIManager.OnCurrentWindowChange += Fresh;
 			#region 动态创建时设置父物体并隐藏
-			if (transform.parent == null)
-			{
-				if (!ParentPanel.IsNull())
-				{
+			if (transform.parent == null) {
+				if (!ParentPanel.IsNull()) {
 					var parent = QUIManager.Load(ParentPanel)?.transform;
-					if (parent == null)
-					{
+					if (parent == null) {
 						QDebug.LogWarning("找不到父页面[" + ParentPanel + "]");
 					}
-					else if (parent != this)
-					{
+					else if (parent != this) {
 						var scale = RectTransform.localScale;
 						RectTransform.SetParent(parent, false);
 						RectTransform.localScale = scale;
-						if (RectTransform.anchorMin == Vector2.zero && RectTransform.anchorMax == Vector2.one)
-						{
+						if (RectTransform.anchorMin == Vector2.zero && RectTransform.anchorMax == Vector2.one) {
 							RectTransform.offsetMin = Vector2.zero;
 							RectTransform.offsetMax = Vector2.zero;
 						}
 					}
 				}
-				if (transform.parent == null)
-				{
+				if (transform.parent == null) {
 					transform.parent = FindFirstObjectByType<Canvas>()?.transform;
 				}
 				HideAndComplete();
 			}
-			else if (IsShow)
-			{
+			else if (IsShow) {
 				initIndex = transform.GetSiblingIndex();
 			}
 			#endregion
 			QUIManager.Add(this);
-			if (gameObject.activeSelf != IsShow)
-			{
+			if (gameObject.activeSelf != IsShow) {
 				gameObject.SetActive(IsShow);
 			}
 		}
-		protected virtual void OnDestroy()
-		{
-			if (_instance == this)
-			{
+		protected virtual void OnDestroy() {
+			if (_instance == this) {
 				_instance = null;
 			}
 			QUIManager.OnCurrentWindowChange -= Fresh;
 			QUIManager.Remove(this);
 		}
-		protected virtual void OnShow()
-		{
-			try
-			{
-				if (initIndex < 0)
-				{
+		protected virtual void OnShow() {
+			try {
+				if (initIndex < 0) {
 					transform.SetAsLastSibling();
 				}
-				else
-				{
+				else {
 					transform.SetSiblingIndex(initIndex);
 					initIndex = -1;
 				}
-				if (Application.isPlaying)
-				{
-					if (isModalWindow)
-					{
+				if (Application.isPlaying) {
+					if (isModalWindow) {
 						QUIManager.ModalWindowPush(this);
 					}
 					OnFresh();
 				}
 			}
-			catch (System.Exception e)
-			{
+			catch (System.Exception e) {
 				QDebug.LogError(this + " " + nameof(OnEnable) + " 出错：" + e);
 			}
 		}
-		protected virtual void OnHide()
-		{
-			try
-			{
-				if (isModalWindow)
-				{
+		protected virtual void OnHide() {
+			try {
+				if (isModalWindow) {
 					QUIManager.ModalWindowRemove(this);
 				}
 			}
-			catch (System.Exception e)
-			{
+			catch (System.Exception e) {
 				QDebug.LogError(this + " " + nameof(OnDisable) + " 出错：" + e);
 			}
 		}
-		protected virtual void OnEnable()
-		{
+		protected virtual void OnEnable() {
 			OnShow();
 		}
-		protected virtual void OnDisable()
-		{
+		protected virtual void OnDisable() {
 			OnHide();
 		}
-		private void Fresh(QUI window)
-		{
-			if (this == null) return;
+		private void Fresh(QUI window) {
+			if (this == null)
+				return;
 			var value = window == null || this.Equals(window) || transform.ParentHas(window.RectTransform);
-			if (value)
-			{
-				if (IsShow)
-				{
+			if (value) {
+				if (IsShow) {
 					Group.interactable = true;
-					if (isModalWindow)
-					{
+					if (isModalWindow) {
 						Group.blocksRaycasts = true;
 					}
 					OnFresh();
 				}
 			}
-			else
-			{
+			else {
 				Group.interactable = false;
-				if (isModalWindow)
-				{
+				if (isModalWindow) {
 					Group.blocksRaycasts = false;
 				}
 			}
@@ -328,8 +262,7 @@ namespace QTool.UI
 		/// <summary>
 		///  主页面切换时调用 刷新数据
 		/// </summary>
-		public virtual void OnFresh()
-		{
+		public virtual void OnFresh() {
 
 		}
 		/// <summary>
@@ -348,99 +281,66 @@ namespace QTool.UI
 		protected virtual async Task SwitchAsync(bool IsShow)
 #pragma warning restore CS1998 // 异步方法缺少 "await" 运算符，将以同步方式运行
 		{
-			if (this == null) return;
+			if (this == null)
+				return;
 			this.IsShow = IsShow;
-			if (IsShow)
-			{
+			if (IsShow) {
 				gameObject.SetActive(true);
 			}
 #if QTween
-			if (showAnim != null)
-			{
+			if (showAnim != null) {
 				var animTask = showAnim.PlayAsync(IsShow);
-				if (await animTask.IsCancel())
-				{
+				if (await animTask.IsCancel()) {
 					return;
 				}
-				if (animTask.Exception != null)
-				{
+				if (animTask.Exception != null) {
 					Debug.LogError("播放页面" + this + "动画出错 " + animTask.Exception);
 				}
-				if (IsShow != base.IsShow)
-				{
+				if (IsShow != base.IsShow) {
 					return;
 				}
-				if (this != null && Application.IsPlaying(this))
-				{
+				if (this != null && Application.IsPlaying(this)) {
 					gameObject.SetActive(IsShow);
 				}
 			}
 			else
 #endif
 			{
-				if (this == null) return;
+				if (this == null)
+					return;
 				Group.interactable = IsShow;
-				if (isModalWindow)
-				{
+				if (isModalWindow) {
 					Group.blocksRaycasts = IsShow;
 				}
 				Group.alpha = IsShow ? 1 : 0;
 			}
-			if (!this.IsShow)
-			{
-				if (Application.IsPlaying(this))
-				{
+			if (!this.IsShow) {
+				if (Application.IsPlaying(this)) {
 					gameObject.SetActive(false);
 				}
 			}
 			gameObject.SetDirty();
 		}
-		public override async Task ShowAsync()
-		{
+		public override async Task ShowAsync() {
 			await SwitchAsync(true).Run();
 		}
 
-		public override async Task HideAsync()
-		{
+		public override async Task HideAsync() {
 			await SwitchAsync(false).Run();
 		}
-		protected virtual void Complete()
-		{
+		protected virtual void Complete() {
 #if QTween
 			showAnim?.Complete();
 #endif
 		}
-		public void ShowAndComplete()
-		{
+		public void ShowAndComplete() {
 			Show();
 			Complete();
 		}
-		public void HideAndComplete()
-		{
+		public void HideAndComplete() {
 			Hide();
 			Complete();
 		}
 		#endregion
-	}
-
-	public abstract class QUI<T, TData> : QUI<T> where T : QUI<T, TData>
-	{
-		public virtual TData Data { get; protected set; }
-		protected override void Awake()
-		{
-			base.Awake();
-			QEventManager.Register<TData>(GetType().Name + "_" + nameof(Show) + "_" + typeof(TData).Name, Show);
-		}
-		public virtual void Show(TData data)
-		{
-			Data = data;
-			Show();
-		}
-		protected override void OnHide()
-		{
-			base.OnHide();
-			Data = default;
-		}
-
 	}
 }
--- Runtime/Core/QUIManager.cs
+++ Runtime/Core/QUIManager.cs
@@ -1,72 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using QTool;
-using UnityEngine.Serialization;
-using QTool.Inspector;
 using System.Threading.Tasks;
-using UnityEngine.SceneManagement;
-namespace QTool.UI
-{
+namespace QTool.UI {
 
 	/// <summary>
 	/// UI管理器
 	/// </summary>
-	public static class QUIManager
-	{
+	public static class QUIManager {
 		#region 管理
 		private static QDictionary<string, QUI> UIList = new QDictionary<string, QUI>();
 		public static event System.Action<QUI> OnCurrentWindowChange;
 		/// <summary>
 		/// 注册UI到管理器
 		/// </summary>
-		internal static void Add(QUI panel)
-		{
+		internal static void Add(QUI panel) {
 			UIList[panel.QName()] = panel;
 		}
-		internal static void Remove(QUI panel)
-		{
+		internal static void Remove(QUI panel) {
 			var key = panel.name;
-			if (!key.IsNull() && UIList.ContainsKey(key))
-			{
+			if (!key.IsNull() && UIList.ContainsKey(key)) {
 				var ui = UIList[key];
-				if (ui == panel)
-				{
+				if (ui == panel) {
 					UIList.RemoveKey(key);
 				}
 			}
 		}
 		public static List<QUI> ModalWindowStack { get; private set; } = new List<QUI>();
-		internal static void ModalWindowPush(QUI window)
-		{
-			if (ModalWindowStack.StackPeek() == window)
-			{
+		internal static void ModalWindowPush(QUI window) {
+			if (ModalWindowStack.StackPeek() == window) {
 				return;
 			}
-			if (ModalWindowStack.Contains(window))
-			{
+			if (ModalWindowStack.Contains(window)) {
 				ModalWindowStack.Remove(window);
 			}
 			ModalWindowStack.Push(window);
 			OnCurrentWindowChange?.Invoke(ModalWindowStack.StackPeek());
 		}
 
-		internal static void ModalWindowRemove(QUI window)
-		{
-			if (ModalWindowStack.Count == 0 || !ModalWindowStack.Contains(window)) return;
+		internal static void ModalWindowRemove(QUI window) {
+			if (ModalWindowStack.Count == 0 || !ModalWindowStack.Contains(window))
+				return;
 			ModalWindowStack.Remove(window);
 			OnCurrentWindowChange?.Invoke(ModalWindowStack.StackPeek());
 		}
-		internal static QUI Load(string key)
-		{
-			if (!QTool.IsPlaying) return null;
-			if (UIList[key] == null)
-			{
+		internal static QUI Load(string key) {
+			if (!QTool.IsPlaying)
+				return null;
+			if (UIList[key] == null) {
 				QDebug.Begin("动态创建" + nameof(QUI) + "<" + key + ">");
 				var prefab = UI_Prefab.Load(key);
-				if (prefab == null)
-				{
+				if (prefab == null) {
 					QDebug.LogError("不存在UI预制体 [" + key + "]");
 					return null;
 				}
@@ -76,45 +60,45 @@ namespace QTool.UI
 			}
 			return UIList[key];
 		}
-		internal static async Task<QUI> LoadAsync(string key)
-		{
+		internal static async Task<QUI> LoadAsync(string key) {
 			await UI_Prefab.LoadAsync(key);
 			return Load(key);
 		}
 		#endregion
 		#region 枚举接口
-		public static void Show<TData>(this System.Enum key, TData data)
-		{
-			QEventManager.InvokeEvent(key.ToString() + "_" + nameof(Show) + "_" + typeof(TData).Name);
+		public static void Show<TData>(this System.Enum key, TData data) {
+			var ui = Load(key.ToString());
+			if (ui == null)
+				return;
+			if (ui is ISetValue<TData> setData) {
+				setData.SetValue(data);
+			}
+			ui.Show();
 		}
-		public static void Show(this System.Enum key)
-		{
-			if (IsShow(key)) return;
+		public static void Show(this System.Enum key) {
+			if (IsShow(key))
+				return;
 			Load(key.ToString()).Show();
 		}
-		public static void Hide(this System.Enum key)
-		{
-			if (!IsShow(key)) return;
+		public static void Hide(this System.Enum key) {
+			if (!IsShow(key))
+				return;
 			Load(key.ToString()).Hide();
 		}
-		public static bool IsShow(this System.Enum enumKey)
-		{
+		public static bool IsShow(this System.Enum enumKey) {
 			var key = enumKey?.ToString();
-			if (UIList.ContainsKey(key)) return UIList[key.ToString()].GetComponent<QUI>().IsShow;
+			if (UIList.ContainsKey(key))
+				return UIList[key.ToString()].GetComponent<QUI>().IsShow;
 			return false;
 		}
-		public static async Task Switch(this System.Enum enumKey, bool show)
-		{
-			if (show == IsShow(enumKey))
-			{
+		public static async Task Switch(this System.Enum enumKey, bool show) {
+			if (show == IsShow(enumKey)) {
 				return;
 			}
-			if (show)
-			{
+			if (show) {
 				await Load(enumKey.ToString()).ShowAsync();
 			}
-			else
-			{
+			else {
 				await Load(enumKey.ToString()).HideAsync();
 			}
 		}
@@ -122,7 +106,6 @@ namespace QTool.UI
 	}
 
 
-	public class UI_Prefab : QPrefabLoader<UI_Prefab>
-	{
+	public class UI_Prefab : QPrefabLoader<UI_Prefab> {
 	}
 }
